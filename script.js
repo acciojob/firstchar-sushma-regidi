25dce86467da506adb9338be8031b2035da49b93
sushma-regidi <regidisushma@gmail.com>
2025-10-03 12:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  // Iterate through each character of the string
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i]; // Return the first non-space character
+    }
+  }
+  return ''; // Return empty string if no non-space char found
 }
 
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+// Test cases
+console.log(firstChar(' Rosa Parks ')); // 'R'
+console.log(firstChar(' Hello World ')); // 'H'
+console.log(firstChar(' ')); // ''
+console.log(firstChar('')); // ''
+
